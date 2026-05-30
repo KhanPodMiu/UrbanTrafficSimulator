6295f9fd0973017c93b1640768631461736c95d7
KhanPodMiu <miupod.3006@gmail.com>
2026-05-30 17:35:53 +0700

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
+#include <SDL2/SDL.h>
 #include <iostream>
 
-using namespace std;
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << '\n';
+        return 1;
+    }
 
-int main()
-{
-    cout << "Hello world";
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL2 Test",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        std::cerr << "Window Error: " << SDL_GetError() << '\n';
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
