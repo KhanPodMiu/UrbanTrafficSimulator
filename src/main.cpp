7cfd5b7b0544f3b61385f84da4f7f95c20d47f9c
KhanPodMiu <miupod.3006@gmail.com>
2026-06-01 19:58:57 +0700

    review json

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,29 @@
 
 
 int main() {
-    std::string file_path = "assets/maps/data.json";
-    std::ifstream file(file_path, std::ifstream::binary);
+    std::string file_name = "assets/maps/data.json";
+    std::ifstream file(file_name, std::ifstream::binary);
 
-    if (!file.is_open()) {
-        std::cerr << "Error: Could not open the file " << file_path << std::endl;
+    if(!file.is_open()){
+        std::cerr << "Error to open " << file_name << std::endl;
         return 1;
     }
 
     Json::Value data;
     Json::CharReaderBuilder readerBuilder;
-    std::string errs;
+    std::string err;
 
-    if (!Json::parseFromStream(readerBuilder, file, &data, &errs)) {
-        std::cerr << "Error parsing JSON: " << errs << std::endl;
+    if(!Json::parseFromStream(readerBuilder, file, &data, &err)){
+        std::cerr << err;
         return 1;
     }
-    
-    std::string school_name = data["name"].asString();
-    std::cout << "School Name: " << school_name << std::endl;
-    
-    std::string city = data["location"]["city"].asString();
-    std::cout << "City: " << city << std::endl;
 
-    std::string first_student_name = data["students"][0]["name"].asString();
-    std::cout << "First Student's Name: " << first_student_name << std::endl;
-
-    const Json::Value& students = data["students"];
+    const Json::Value students = data["students"];
     for(const auto& student : students){
-        std::cout << student["name"].asString() << std::endl;
+        std::cout << student["name"].asString() << " " << student["age"].asInt() << '\n';
     }
+
+    file.close();
 
     return 0;
 }
