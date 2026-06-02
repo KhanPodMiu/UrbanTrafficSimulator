427a276010384400cacf25a5c8a5b98012a7d901
KhanPodMiu <miupod.3006@gmail.com>
2026-06-02 19:26:23 +0700

    refresh main

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,6 @@
 
 
 int main() {
-    std::string file_name = "assets/maps/data.json";
-    std::ifstream file(file_name, std::ifstream::binary);
-
-    if(!file.is_open()){
-        std::cerr << "Error to open " << file_name << std::endl;
-        return 1;
-    }
-
-    Json::Value data;
-    Json::CharReaderBuilder readerBuilder;
-    std::string err;
-
-    if(!Json::parseFromStream(readerBuilder, file, &data, &err)){
-        std::cerr << err;
-        return 1;
-    }
-
-    const Json::Value students = data["students"];
-    for(const auto& student : students){
-        std::cout << student["name"].asString() << " " << student["age"].asInt() << '\n';
-    }
-
-    file.close();
 
     return 0;
 }
