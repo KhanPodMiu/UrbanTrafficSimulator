ebbed6d29d27487ef8245cf1685436af0e953a08
KhanPodMiu <miupod.3006@gmail.com>
2026-06-03 18:25:04 +0700

    fix : remove all temporary sdl2 in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,8 @@
-#define SDL_MAIN_HANDLED
-#include <SDL2/SDL.h>
 #include <iostream>
 
 int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << '\n';
-        return 1;
-    }
 
-    SDL_Window* window = SDL_CreateWindow(
-        "SDL2 Test",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
-        SDL_WINDOW_SHOWN
-    );
-
-    if (!window) {
-        std::cerr << "Window Error: " << SDL_GetError() << '\n';
-        SDL_Quit();
-        return 1;
-    }
-
-    bool running = true;
-    SDL_Event event;
-
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
-        }
-    }
-
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    std::cout << "Hello world";
 
     return 0;
 }
