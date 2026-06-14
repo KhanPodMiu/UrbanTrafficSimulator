a178d347d7c7f398257e75227bf5a7293f52c1d4
hchinhan <cucmo845@gmail.com>
2026-06-14 22:20:38 +0700

    feat: Add RouteRequest and RouteRessult data model

--- include/simulation/RouteRequest.hpp
+++ include/simulation/RouteRequest.hpp
@@ -6,7 +6,6 @@ struct RouteRequest {
     std::string startIntersectionID;
     std::string destinationIntersectionID;
 
-    // Constructor mặc định và có tham số
     RouteRequest() = default;
     RouteRequest(const std::string& start, const std::string& dest)
         : startIntersectionID(start), destinationIntersectionID(dest) {}
