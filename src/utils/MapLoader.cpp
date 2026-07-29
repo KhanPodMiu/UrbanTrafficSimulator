bee1a610bf03780ecae471eeb7d2e59c38783e5d
hchinhan <cucmo845@gmail.com>
2026-07-30 00:32:33 +0700

    feat: add logic for render banned road

--- src/utils/MapLoader.cpp
+++ src/utils/MapLoader.cpp
@@ -31,11 +31,17 @@ bool MapLoader::loadFromJson(const std::string& filename, Graph& graph){
     }
 
     const Json::Value intersections = data["intersections"];
-    for(const auto& intersection : intersections){
-        if(
-        !graph.addIntersection(std::make_shared<Intersection>(intersection["id"].asString(), intersection["x"].asInt(), intersection["y"].asInt()))
-        )
-        {
+    for (const auto& intersection : intersections) {
+        std::string id = intersection["id"].asString();
+        if (graph.getIntersection(id) != nullptr) {
+            continue; 
+        }
+        auto newIntersection = std::make_shared<Intersection>(
+            id, 
+            intersection["x"].asInt(), 
+            intersection["y"].asInt()
+        );
+        if (!graph.addIntersection(newIntersection)) {
             std::cerr << "Duplicate intersection ID..... Please check your Json map\n";
             return false;
         }
@@ -44,6 +50,15 @@ bool MapLoader::loadFromJson(const std::string& filename, Graph& graph){
     const Json::Value Roads = data["roads"];
     for(const auto& road : Roads){
         std::string roadID = road["id"].asString();
+        bool isBanned = false;
+        if (road.isMember("isVIPExclusive")) {
+            isBanned = road["isVIPExclusive"].asBool();
+        }
+        std::shared_ptr<Road> existingRoad = graph.getRoad(roadID);
+        if (existingRoad != nullptr) {
+            existingRoad->setVIPExclusive(isBanned);
+            continue;
+        }
 
         auto src = graph.getIntersection(road["source"].asString());
         auto des = graph.getIntersection(road["destination"].asString());
@@ -53,10 +68,10 @@ bool MapLoader::loadFromJson(const std::string& filename, Graph& graph){
             return false;
         }
 
-        if(
-            !graph.addRoad(std::make_shared<Road>(roadID, src.get(), des.get(), Road::MAX_SPEED_LIMIT))
-        )
-        {
+        auto newRoad = std::make_shared<Road>(roadID, src.get(), des.get(), Road::MAX_SPEED_LIMIT);
+        newRoad->setVIPExclusive(isBanned); 
+
+        if (!graph.addRoad(newRoad)) {
             std::cerr << "Duplicate road ID\n";
             return false;
         }
