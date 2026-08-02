5e658f4fe80f025841e9ae5033112157ffa2e0c3
KhanPodMiu <miupod.3006@gmail.com>
2026-08-02 09:41:14 +0700

    refactor: separate handle event to eventManager

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "utils/event_handling.hpp"
 
 #include "utils/MapLoader.hpp"
+#include "utils/EventManager.hpp"
 #include "visualization/VisualizationEngine.hpp"
 
 #include "simulation/WorldClock.hpp"
@@ -204,86 +205,8 @@ int main(int argc, char* argv[]) {
 
         Uint64 frameStart = SDL_GetPerformanceCounter();
 
-        // --- Main game loop sections (event handling) ---
-        while (SDL_PollEvent(&event)) {
-            const bool heatMapControlHandled =
-                visualizationEngine.handleTrafficHeatMapEvent(event);
-
-            if (!heatMapControlHandled)
-                handleInput(event, appContext);
-
-            const PanelCommand panelCommand =
-                statisticsPanel.handleEvent(event);
-
-            switch (panelCommand)
-            {
-                case PanelCommand::Start:
-                    clock.start();
-                    break;
-
-                case PanelCommand::Pause:
-                    clock.pause();
-                    break;
-
-                case PanelCommand::Restart:
-                    clock.reset();
-                    vehicleManager.reset();
-                    TrafficLightManager::getInstance().reset();
-                    entityInfoPanel.clearSelection();
-                    clock.start();
-                    break;
-
-                case PanelCommand::SpeedChanged:
-                    clock.setSpeedMultiplier(
-                        statisticsPanel.getSelectedSpeedMultiplier());
-                    break;
-
-                case PanelCommand::AlgorithmChanged:
-                case PanelCommand::None:
-                    break;
-            }
-
-            if (event.type == SDL_MOUSEBUTTONUP &&
-                event.button.button == SDL_BUTTON_LEFT &&
-                !heatMapControlHandled &&
-                !wasDragAction() &&
-                event.button.x >= Config::PANEL_WIDTH &&
-                !entityInfoPanel.containsPoint(
-                    event.button.x,
-                    event.button.y))
-            {
-                const std::shared_ptr<Vehicle> selectedVehicle =
-                    visualizationEngine.pickVehicle(
-                        camera,
-                        vehicleManager.getVehicles(),
-                        event.button.x,
-                        event.button.y);
-
-                if (selectedVehicle)
-                {
-                    entityInfoPanel.selectVehicle(selectedVehicle);
-                }
-                else
-                {
-                    const std::shared_ptr<Intersection>
-                        selectedIntersection =
-                            visualizationEngine.pickIntersection(
-                                camera,
-                                event.button.x,
-                                event.button.y);
-
-                    if (selectedIntersection)
-                    {
-                        entityInfoPanel.selectIntersection(
-                            selectedIntersection);
-                    }
-                    else
-                    {
-                        entityInfoPanel.clearSelection();
-                    }
-                }
-            }
-        }
+        //just separated it again, dont merge them toghether plssss!!!!
+        EventManager::processEvents(appContext, clock, statisticsPanel, entityInfoPanel, vehicleManager, camera, visualizationEngine);
 
         // --- Main game loop sections (simulation update) ---
         clock.update();
